932566d3657a8ea27dd16dfdbe679ac37f40f204
Ekene <ernestoekeneuteh@gmail.com>
2022-04-10 15:06:06 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print lowercase alphabet excluding e and q
+ *
+ */
+int main (void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; +cc)
+{
+if (c != 'a' && c != 'q')
+putchar(c)
+}
+
+putchar('\n');
+
+return (0);
+}
