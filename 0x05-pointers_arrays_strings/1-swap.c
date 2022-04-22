6e622de47eaaa276b0437086cf89b8bc9f8f3c87
Ekene <ernestoekeneuteh@gmail.com>
2022-04-22 14:57:49 +0000

    second commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping 2 integers using a pointer
+ * @a:integer
+ * @0:integer
+ * Return:void
+ */
+void swap_int(int *a, int *b);
+{
+	x = *a;
+	*a = *b;
+	*b = x;
+
+	return;
+}
