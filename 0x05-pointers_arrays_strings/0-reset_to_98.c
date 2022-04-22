7608494552658391ce0111a6a42bf0832e1f59d2
Ekene <ernestoekeneuteh@gmail.com>
2022-04-22 14:38:50 +0000

    task 0 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - entry point
- * reset to 98 - change the value of integer using pointer
+ * reset_to_98 - change the value of integer using pointer
  * @n: pointer
  * Return:void
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
