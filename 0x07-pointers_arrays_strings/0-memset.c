acdf913e01cd76c277024186990b86014b89f32e
Ekene <ernestoekeneuteh@gmail.com>
2022-04-27 18:40:49 +0000

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,18 +8,15 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
+	unsigned int i;
 
-unsigned int i;
+	/*Declaring For*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
 
-/*Delacring FOR*/
+	} /*END FOR*/
 
-for (i = 0; i < n; i++)
-{
-	*(s + i) = b; /*add 1 position s*/
-
-} /*END FOR*/
-
-return (s);
+	return (s);
 }
