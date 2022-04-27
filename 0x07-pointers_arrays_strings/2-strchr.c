daa3c94eed0a4144869b8b7141da56334113ba83
Ekene <ernestoekeneuteh@gmail.com>
2022-04-27 18:57:35 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,22 +9,20 @@
 
 char *_strchr(char *s, char c)
 {
+	while (*s != '\0') /*Declaring WHILE*/
+	{
+		if (*s == c) /*if s == c*/
+		{
+			return (s);
+		}
 
-while (*s != '\0') /*Declaring WHILE*/
-if (*s == c) /*if s ==  c*/
-{
-return (s); /*return s*/
-}
+		++s;
+	}
 
-++s;
-}
+	if (*s == c)
+	{
+		return (s);
+	}
 
-if (*s == c)
-{
-return (s);
-
-}
-
-
-return (0);/* values null*/
+	return (0); /*values null*/
 }
